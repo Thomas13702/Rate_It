de83e1e42cd89c2dde644b6aec432132eaf59cf8
Thomas13702 <thomaspritchard137@btinternet.com>
2021-06-24 23:38:44 +0100

    Added API and frontend

---
+++ pages/api/image.js
@@ -0,0 +1,25 @@
+import Scraper from "images-scraper";
+import randomWords from "random-words";
+
+export default async (req, res) => {
+  //   res.status(200).json({ name: "John Doe" });
+
+  if (req.method === "GET") {
+    const google = new Scraper({
+      puppeteer: {
+        headless: true,
+      },
+    });
+
+    const results = await google.scrape(randomWords(), 1);
+    res.json(results);
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+};
+
+// const handler = (req, res) => {
+//   return res.json({ hello: "world!" });
+// };
+// export default handler;
